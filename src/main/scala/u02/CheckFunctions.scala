37c0e224a44337ec3d800d3bb74189e564b97f04
zanetti33 <lorenzo.zanetti5@studio.unibo.it>
2024-03-15 18:06:34 +0100

    trying to do generic lambda

---
+++ src/main/scala/u02/CheckFunctions.scala
@@ -0,0 +1,5 @@
+package u02
+
+object CheckFunctions {
+  
+}
--- src/main/scala/u02/Neg.scala
+++ src/main/scala/u02/Neg.scala
@@ -5,10 +5,14 @@ object Neg extends App {
     def neg[X](predicate: X => Boolean): X => Boolean =
         x => !predicate(x)
 
+    
+    val negAsLambda: ([X] => Boolean) => ([X] => Boolean) = predicate => (x => !predicate(x))
 
     println(neg(_ == "")("pippo") && !neg(_ == "")(""))
+    println(negAsLambda(_ == "")("pippo") && !negAsLambda(_ == "")(""))
 
     var positive: Int => Boolean = _ >= 0
     println(neg(positive)(-6) && !neg(positive)(17))
+    println(negAsLambda(positive)(-6) && !negAsLambda(positive)(17))
 
 }
